231fc45b9fa495d78b220578789131b98d630946
Cbriody <c.briody@federation.edu.au>
2026-07-06 14:04:52 +1000

    app.js update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
 import "../css/app.css";
-import logoUrl from '../images/01_CeRDI_lockups_CMYK_acronym-400x400.png'
+import logoUrl from '../images/01_CeRDI_lockups_CMYK_acronym-400x400.png';
+import secondaryLogoUrl from '../images/CeRDI-acronym-and-fed-flag-2-solid-white-bg.png';
+
 window.logoUrl = logoUrl
+window.secondaryLogoUrl = secondaryLogoUrl
